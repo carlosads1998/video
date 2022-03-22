b6f213b98b766001528c6dfc00b4524d56eb244d
carlosads1998 <carlos.ads1998@gmail.com>
2022-03-22 13:15:44 -0300

    s3

--- video/settings.py
+++ video/settings.py
@@ -132,5 +132,5 @@ AWS_STORAGE_BUCKET_KEY= 'video-cads'
 
 AWS_S3_FILE_OVERWRITE= False
 AWS_DEFAULT_ACL= None
-DEFAULT_FILE_STORAGE= 'storages.backends.s3boto.S3Boto3Storage'
+DEFAULT_FILE_STORAGE= 'storages.backends.s3boto3.S3Boto3Storage'
 django_heroku.settings(locals())
