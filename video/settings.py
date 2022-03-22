df202a8559831f60a16669bbd2768a5ad42bc64a
carlosads1998 <carlos.ads1998@gmail.com>
2022-03-22 12:42:25 -0300

    pronto

--- video/settings.py
+++ video/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '%9g-b-ff2ik3x%hzd&nqwoy13wemyq9gk^gxk)un4yl4y9)6sa'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://video-teste.herokuapp.com/']
 
 
 # Application definition
@@ -132,3 +133,5 @@ AWS_STORAGE_BUCKET_KEY= 'video-cads'
 AWS_S3_FILE_OVERWRITE= False
 AWS_DEFAULT_ACL= None
 DEFAULT_FILE_STORAGE= 'storage.backends.s3boto.S3Boto3Storage'
+
+django_heroku.settings(locals())
